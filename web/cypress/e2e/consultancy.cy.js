02889f6ca90bdf402bf3d21c86682f8694a9f2b6
Leonardo Tiodozio de França <leonardotiodozio192@gmail.com>
2025-10-01 18:11:41 -0300

    Começando a automação na aba de Consultoria

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -10,5 +10,43 @@ describe('Formulário de consultoria', ()=>{
     cy.get('input[placeholder="(00) 00000-0000"]')
         .type('55999999999')
         .should('have.value', '(55) 99999-9999')
-  })  
+
+    cy.contains('label', 'Tipo de Consultoria')
+      .parent()
+      .find('select')
+      .select('Individual')
+
+    cy.contains('label', 'Pessoa Física')
+      .find('input')
+      .click()
+      .should('be.checked')
+
+    cy.contains('label', 'Pessoa Jurídica')
+      .find('input')
+      .should('be.not.checked')
+
+    cy.contains('label', 'CPF')
+      .parent()
+      .find('input')
+      .type('16103899052')
+      .should('have.value', '161.038.990-52')
+
+    const discoveryChannels = [
+      'Instagram',
+      'LinkedIn',
+      'Udemy',
+      'YouTube',
+      'Indicação de Amigo'
+    ]
+
+    discoveryChannels.forEach((channel)=>{
+      cy.contains('label', channel)
+      .find('input')
+      .check()
+      .should('be.checked')
+    })
+  
+    cy.get('input[type="file"]')
+      .selectFile('./cypress/fixtures/document.pdf', { force: true })
+  })
 })
