323bd6c149376e7c0adf94761d1fe46cc9dd0b14
Leonardo Tiodozio <leonardotiodozio192@gmail.com>
2025-10-03 18:02:09 -0300

    Finalização da automação estilo iFrame e inialização em Kanban e PDF

---
+++ web/cypress/e2e/download.cy.js
@@ -0,0 +1,5 @@
+describe('Download PDF', () => {
+    it('Deve validar o conteudo do recibo em PDF', () => {
+        cy.visit('https://araucariageneticabovina.com.br/arquivos/servico/pdfServico_57952bf8ca7af_24-07-2016_17-58-32.pdf')
+    })
+})
--- web/cypress/e2e/iframe.cy.js
+++ web/cypress/e2e/iframe.cy.js
@@ -7,6 +7,17 @@ describe('Rodar o video', ()=> {
         
         cy.get('iframe[title="Video Player"]')
             .should('exist')
-            .its('0.contentDocument.body') 
+            .its('0.contentDocument.body')
+            .then(cy.wrap)
+            .as('iFramePlayer')
+
+        cy.get('@iFramePlayer')
+            .find('.play-button')
+            .click()
+            
+        cy.get('@iFramePlayer')
+            .find('.pause-button')
+            .should('be.visible')
+
     })
 })
